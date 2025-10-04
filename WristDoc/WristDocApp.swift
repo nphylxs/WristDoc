086f09b5b4e868eebec60cf773fcccb3c42ba97d
nphylxs <130030842+nphylxs@users.noreply.github.com>
2025-10-04 14:55:36 -0400

    Initial Commit

---
+++ WristDoc/WristDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristDocApp.swift
+//  WristDoc
+//
+//  Created by Divyamshu Shrestha on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WristDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
